1bd13c1d19b778e9bf93d4e9f12e2290b79694c5
pop-cm <popcm@live.ca>
2023-11-06 13:36:31 -0500

    Optimize loading of database config file

--- src/main/java/shop/util/DatabaseConnector.java
+++ src/main/java/shop/util/DatabaseConnector.java
@@ -3,26 +3,36 @@ package shop.util;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Objects;
 
 public class DatabaseConnector {
+
+    private static final JsonObject dbInfo;
+
+    // Load data access configuration file only once
+    static {
+        try (InputStream inputStream = DatabaseConnector.class.getClassLoader().getResourceAsStream("db_config.json");
+             InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(inputStream))) {
+            dbInfo = JsonParser.parseReader(reader).getAsJsonObject();
+        } catch (NullPointerException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final String url = dbInfo.get("db_url").getAsString();
+    private static final String user = dbInfo.get("db_user").getAsString();
+    private static final String password = dbInfo.get("db_password").getAsString();
+
     public static Connection getConnection() {
         try {
-            // Read data access details from configuration file
-            String filepath = DatabaseConnector.class.getClassLoader().getResource("db_config.json").getPath();
-            FileReader reader = new FileReader(filepath);
-            JsonObject dbInfo = JsonParser.parseReader(reader).getAsJsonObject();
-            String url =  dbInfo.get("db_url").getAsString();
-            String user =  dbInfo.get("db_user").getAsString();
-            String password = dbInfo.get("db_password").getAsString();
-
-            // Establish database connection based on the above
             return DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException(e);
         }
     }
 
